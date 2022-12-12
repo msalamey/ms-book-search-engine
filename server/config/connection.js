df62dbc6028b76e2bc2c609a33f44d5f3cdaa633
msalamey <m.salamey23@gmail.com>
2022-12-12 02:00:38 -0500

    reverted some changes in code

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ikeys0167:<ikey2233>@cluster0.akff4q6.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
+
+module.exports = mongoose.connection;
