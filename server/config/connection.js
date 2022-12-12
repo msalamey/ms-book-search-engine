d7625caaf8a189a9f7a27889474a594d38e884d6
msalamey <m.salamey23@gmail.com>
2022-12-12 01:39:52 -0500

    minor change in code

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', 'mongodb+srv://ikeys0167:<password>@cluster0.akff4q6.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', 'mongodb+srv://ikeys0167:<ikey2233>@cluster0.akff4q6.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
