4aa9db688d75e802e8f39e70d4beff722c2cd4d6
msalamey <m.salamey23@gmail.com>
2022-12-12 00:43:33 -0500

    changes to node modules

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/'));
 });
 
 // create new instance of an Apollo server with Graphql schema
